34d29b2e08346938398f5cd8bade45ae46a7f574
Austonst <austonst@gmail.com>
2025-02-19 17:07:09 -0700

    Revert "chore: reverted changes to dockerfile + tls dependencies"
    This reverts commit 25d777ebed9b3201c3171e1f62fa7aa446b72e55.

--- crates/common/src/config.rs
+++ crates/common/src/config.rs
@@ -183,6 +183,8 @@ pub struct PostgresConfig {
     pub password: String,
     pub region: i16,
     pub region_name: String,
+    pub ssl_mode: Option<String>,
+    pub cert_file_pem: Option<String>,
 }
 
 #[derive(Serialize, Deserialize, Clone, Default)]
