ff8d350034d88f8b7ac37c93e33f90328f13b15e
DigiDr <max@birge.me>
2025-10-21 23:30:47 +0100

    fix: failsafe slot transition and tidy up

--- crates/relay/src/main.rs
+++ crates/relay/src/main.rs
@@ -122,6 +122,7 @@ async fn run(config: RelayConfig, keypair: BlsKeypair) -> eyre::Result<()> {
             config.k8s_leader_election.clone(),
             is_leader.clone(),
             &current_slot_info,
+            chain_info.clone(),
             &local_cache,
         )
         .await
