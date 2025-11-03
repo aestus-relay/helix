80479e9d0a2e2d3ddc68247751d4260655ec53cc
DigiDr <max@birge.me>
2025-11-03 10:36:14 +0000

    chore: fix tracing of slot number in submit_bid

--- crates/relay/src/auctioneer/worker.rs
+++ crates/relay/src/auctioneer/worker.rs
@@ -157,7 +157,7 @@ impl SubWorker {
                         bid_adjustment_data,
                     )) => {
                         tracing::Span::current()
-                            .record("bid_slot", tracing::field::display(submission.bid_slot()));
+                            .record("slot", tracing::field::display(submission.bid_slot()));
                         tracing::Span::current()
                             .record("block_hash", tracing::field::display(submission.block_hash()));
                         tracing::Span::current().record(
