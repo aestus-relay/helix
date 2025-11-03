53ae58dc1dabf048dfc80a87b7ceffa410bd0788
DigiDr <max@birge.me>
2025-11-03 12:42:39 +0000

    chore: add Json and JsonConsole log formatters

--- crates/common/src/config.rs
+++ crates/common/src/config.rs
@@ -384,7 +384,14 @@ pub struct PrimevConfig {
 pub enum LoggingConfig {
     #[default]
     Console,
+    JsonConsole,
     File {
+        dir_path: PathBuf,
+        file_name: String,
+        /// OpenTelemetry server URL
+        otlp_server: Option<Url>,
+    },
+    JsonFile {
         dir_path: PathBuf,
         file_name: String,
         /// OpenTelemetry server URL
@@ -397,7 +404,9 @@ impl LoggingConfig {
     pub fn dir_path(&self) -> Option<PathBuf> {
         match self {
             LoggingConfig::Console => None,
+            LoggingConfig::JsonConsole => None,
             LoggingConfig::File { dir_path, .. } => Some(dir_path.clone()),
+            LoggingConfig::JsonFile { dir_path, .. } => Some(dir_path.clone()),
         }
     }
 }
